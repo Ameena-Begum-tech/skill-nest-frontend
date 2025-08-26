7fd5b45db1af490c4416819e61d4da4f580aa7bf
Ameena Begum <ameenabegum140@gmail.com>
2025-08-26 21:11:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import Header from "./Components/header/Header";
+import Header from "./Components/Header/Header";
 import Login from "./Pages/auth/Login";
 import Register from "./Pages/auth/Register";
 import Verify from "./Pages/auth/Verify";
