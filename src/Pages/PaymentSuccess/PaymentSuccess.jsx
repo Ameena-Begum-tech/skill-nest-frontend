e3ee3dff4c999716575f0f75e8ee4416b79aa8b4
Ameena Begum <ameenabegum140@gmail.com>
2025-08-26 20:55:11 +0530

    Update PaymentSuccess.jsx

--- src/Pages/PaymentSuccess/PaymentSuccess.jsx
+++ src/Pages/PaymentSuccess/PaymentSuccess.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./paymentsuccess.css";
+import "./PaymentSuccess.css";
 import { Link, useParams } from "react-router-dom";
 import { FaCheckCircle } from "react-icons/fa";
 
